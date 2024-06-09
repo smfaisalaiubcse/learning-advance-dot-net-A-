0d61cf1dce7a2150f78f02b6bdb84241fb82025e
S M Faisal <smf48406@gmail.com>
2024-06-09 15:29:30 +0600

    f

---
+++ Mid/Class-task/Task-1/Task-1/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_1.Models;
+
+namespace Task_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        Student s1 = new Student()
+        {
+            Id = 1,
+            Name = "S M Faisal",
+            Email = "itssmfaisal@gmail.com",
+            Phone = "+8801305619071",
+            Expertise = "Programming",
+            Degrees = new List<Degree>
+            {
+                new Degree
+                {
+                    DegreeName = "BSc CSE",
+                    Result = "First Class",
+                    PassingYear = 2015,
+                    Group = "CS",
+                    Institution = "XYZ University"
+                },
+                new Degree
+                {
+                    DegreeName = "HSC",
+                    Result = "GPA-5",
+                    PassingYear = 2019,
+                    Group = "Science",
+                    Institution = "XYZ College"
+                }
+            },
+            Projects = new List<Project>
+            {
+                new Project
+                {
+                    ProjectTitle = "Trip_Nest",
+                    Description = "A nest JS project",
+                    Course = "Adv. Programming with web technology"
+                },
+                new Project
+                {
+                    ProjectTitle = "University Management System",
+                    Description = "A Normal PHP, JS project",
+                    Course = "Web technology"
+                }
+            }
+        };
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Adv .Net";
+            ViewBag.CTeacher = "Tanvir";
+
+            ViewBag.Students = new Student[] { s1 };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult EducationDetails(int id)
+        {
+            ViewBag.Students = new Student[] { s1 };
+            return View();
+        }
+        public ActionResult ProjectDetails(int id)
+        {
+            ViewBag.Students = new Student[] { s1 };
+            return View();
+        }
+
+    }
+}
---
+++ Mid/Class-task/Task-1/Task-1/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_1.Models
+{
+    public class Degree
+    {
+        public string DegreeName { get; set; }
+        public string Result { get; set; }
+        public int PassingYear { get; set; }
+        public string Group { get; set; }
+        public string Institution { get; set; }
+    }
+    public class Project
+    {
+        public string ProjectTitle { get; set; }
+        public string Description { get; set; }
+        public string Course { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Degree> Degrees { get; set; }
+        public List<Project> Projects { get; set; }
+        public string Expertise { get; set; }
+
+        public Student()
+        {
+            Degrees = new List<Degree>();
+            Projects = new List<Project>();
+        }
+    }
+}
